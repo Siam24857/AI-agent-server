2e34c7b9d25db1ccbf96718909ce917978b87704
Siam24857 <siamtechofficial1597@gmail.com>
2026-07-18 10:12:06 +0600

    all is done by me

--- api/handler.ts
+++ api/handler.ts
@@ -1,14 +1,4 @@
-const { app } = require('./dist/src/server');
-
-module.exports = async function handler(req, res) {
-  try {
-    await app(req, res);
-  } catch (error) {
-    console.error('Serverless function error:', error);
-    res.status(500).json({
-      success: false,
-      error: 'Internal Server Error',
-      message: error.message
-    });
-  }
+module.exports = function handler(req, res) {
+  const { app } = require('./dist/src/server');
+  app(req, res);
 }
